5d4fb786e92d4a57c529641526535763ad7561d3
NV <nv491571@gmail.com>
2024-10-10 15:41:16 +0530

    html

---
+++ ad verma.js
@@ -0,0 +1,8 @@
+import {add,subtract,PI} from "./advance banya.js";
+import * as jadoo from "./advance banya.js";
+import  multiply  from "./advance banya.js";
+export{add,subtract,PI,multiply};
+console.log("add:",jadoo.add(5,6));
+console.log("subtract:",jadoo.subtract(5,6));
+console.log("PI:",jadoo.PI);
+console.log("multiply:",multiply(5,6));
---
+++ verma.js
@@ -0,0 +1,5 @@
+const check = require("./advance banya.js");
+console.log("add:",check.add(5,6));
+console.log("subtract:",check.subtract(5,6));
+console.log("PI:",check.PI);
+console.log("multiply:",check.multiply(5,6));
